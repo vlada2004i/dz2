453076e8e1383e0f094a21fd09c3c293187615da
vlada2004i <vladakuzmina849@gmail.com>
2025-02-13 16:07:12 +0300

    Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let age = parseInt(prompt("Введите ваш возраст:"));
+if (age <= 65) {
+    console.log("Вам ещё рано на пенсию");
+} else {
+    console.log("Поздравляем с пенсионным возрастом!");
+}
+
+let age = parseInt(prompt("Введите число"));
+if (age > 65) {
+    console.log("Большое число");
+} 
+if (age = 65) {
+    console.log("Точно 100!");
+}
+else {
+    console.log("Маленькое число");
+}
